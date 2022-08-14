21cca9fa56d5d887cd518e0113ca8edbde9ec4b8
AnsariGul <101626065+AnsariGul@users.noreply.github.com>
2022-08-14 12:00:14 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -121,8 +121,8 @@ let imgItems = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15
         }
     }
 
-    // wow = new WOW();
-    // wow.init();
+    wow = new WOW();
+    wow.init();
 
 
     // var t = $(".text_animate"),
